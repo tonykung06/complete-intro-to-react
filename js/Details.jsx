41a328beec04e527fbc45193a859d310df056b4a
tonykung <tonykung06@gmail.com>
2016-04-20 03:07:31 +0800

    ajax request to get imdb data at componentDidMount

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import Header from './Header';
 import {connector} from './Store';
+import axios from 'axios';
 
 const {object, arrayOf} = React.PropTypes;
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      omdbData: {}
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`http://www.omdbapi.com/?i=${this.getCurrentShow(this.props.shows, this.props.params.id).imdbID}`).then(response => {
+      this.setState({omdbData: response.data});
+    }).catch(error => {
+      console.error('axios error', error);
+    });
+  }
+
   getCurrentShow(shows, id) {
     const showArray = shows.filter(item => id === item.imdbID);
 
     return showArray[0];
   }
+
   render() {
     const {title, description, year, poster, trailer} = this.getCurrentShow(this.props.shows, this.props.params.id) || {};
+    let rating;
+
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3 className="video-rating">{this.state.omdbData.imdbRating}</h3>;
+    }
 
     return (
       <div className="container">
@@ -20,7 +43,10 @@ class Details extends React.Component {
           <h1 className="video-title">
             {title}
           </h1>
-          <h1 className="video-year">({year})</h1>
+          <h1 className="video-year">
+            ({year})
+          </h1>
+          {rating}
           <img src={`/public/img/posters/${poster}`} alt="" className="video-poster"/>
           <p className="video-description">{description}</p>
         </div>
